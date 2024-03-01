c81fa63188d12298c1ef3aa8c376f2582528f35a
Marcomagnano93 <marcog.magnano@gmail.com>
2024-03-01 14:36:12 +0100

    FIRST COMMIT: ADD: ex 1 - 2 - 3

---
+++ js/app.js
@@ -0,0 +1,52 @@
+//*******ESERCIZIO 1*******
+
+let number = 100; //number
+
+console.log(number)
+
+
+
+
+//*******ESERCIZIO 2*******
+
+const PI = 3.14 // number
+
+console.log(PI)
+
+
+
+
+//*******ESERCIZIO 2/B*******
+
+//PI = 3.1416; //number
+
+// Non funziona il display in console di PI perchè è una costante (const) e quindi non possiamo riassegnare il suo valore.
+
+// Per ottenere in console 3.1416 secondo me dovremmo fare:
+
+const numberTwo = 0.0016; //number
+
+console.log(PI + numberTwo)
+
+
+
+
+
+//ESERCIZIO 2/C
+
+let radius = 8; //number
+
+let circle = radius * 2 * (PI + numberTwo); //number
+
+console.log("Il valore di circle è: " + circle)
+
+
+
+
+//ESERCIZIO 3
+
+let name = "Carlo"; //string
+
+name = "Marco"; //string
+
+console.log("Il nome è: " + name)
